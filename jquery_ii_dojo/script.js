51dd634904c0aa88bc782874990924cc02c2bdae
Goh Matsumoto <urimaro@gmail.com>
2021-01-10 15:54:16 +0900

    3. Creating an Accordion - 1. Making a Webpage with jQuery

--- jquery_ii_dojo/script.js
+++ jquery_ii_dojo/script.js
@@ -15,4 +15,17 @@ $(function() {
       $(this).find('.text-contents').css('display', 'none');
     }
   );
+
+  $('.faq-list-item').click(function() {
+    $answer = $(this).find('.answer');
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
+    }
+  });
 });
