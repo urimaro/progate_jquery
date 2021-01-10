e5f687a1b791baabfca120bbadc4319bd0a7ff54
Goh Matsumoto <urimaro@gmail.com>
2021-01-10 13:21:35 +0900

    9. Accordions (3) - 3. Practical jQuery (3)

--- jquery_ii/script.js
+++ jquery_ii/script.js
@@ -25,8 +25,12 @@ $(function() {
     var $answer = $(this).find('.answer');
     if ($answer.hasClass('open')) {
       $answer.removeClass('open');
+      $answer.slideUp();
+      $(this).find('span').text('+');
     } else {
       $answer.addClass('open');
+      $answer.slideDown();
+      $(this).find('span').text('-');
     }
   });
 });
