b7dd901f6b68493057f8b61b6f65013e27d488b7
Goh Matsumoto <urimaro@gmail.com>
2021-01-10 15:40:49 +0900

    2. Lesson List - 1. Making a Webpage with jQuery

--- jquery_ii_dojo/script.js
+++ jquery_ii_dojo/script.js
@@ -6,4 +6,13 @@ $(function() {
   $('#close-modal').click(function() {
     $('#signup-modal').css('display', 'none');
   });
+
+  $('.lesson').hover(
+    function() {
+      $(this).find('.text-contents').css('display', 'block');
+    },
+    function() {
+      $(this).find('.text-contents').css('display', 'none');
+    }
+  );
 });
