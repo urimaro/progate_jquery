2c353369f3b2af858aa867204e5209c18fc96c59
Goh Matsumoto <urimaro@gmail.com>
2021-01-13 08:56:48 +0900

    9. Getting Element Values (2) - 2. Working with Forms

--- jquery_iii/2_script.js
+++ jquery_iii/2_script.js
@@ -1,4 +1,9 @@
 $(function() {
   var title = $('#title').text();
+  var id = $('.section-content p').attr('id');
+  var href = $('#link').attr('href');
+
   $('#title-text').text(title);
+  $('#content-id').text(id);
+  $('#link-href').text(href);
 });
