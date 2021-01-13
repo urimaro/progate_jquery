125dc21c24c6faf1a0ec87325f2e6679b733c5b9
Goh Matsumoto <urimaro@gmail.com>
2021-01-13 23:35:24 +0900

    11. Getting Select Box Values - 2. Working with Forms

--- jquery_iii/3_script.js
+++ jquery_iii/3_script.js
@@ -1,6 +1,9 @@
 $(function() {
   $('#form').submit(function() {
+    var selectValue = $('#select-form').val();
     var textValue = $('#text-form').val();
+
+    $('#output-select').text(selectValue);
     $('#output-text').text(textValue);
     return false;
   });
