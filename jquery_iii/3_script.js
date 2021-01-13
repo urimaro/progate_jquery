4e3e4f30c85ba8ec7408c8fe722f32004ffea29c
Goh Matsumoto <urimaro@gmail.com>
2021-01-13 23:43:44 +0900

    12. Checking Form Input - 2. Working with Forms

--- jquery_iii/3_script.js
+++ jquery_iii/3_script.js
@@ -3,6 +3,12 @@ $(function() {
     var selectValue = $('#select-form').val();
     var textValue = $('#text-form').val();
 
+    if (textValue == '') {
+      $('#error-message').text('理由を記入してください');
+    } else {
+      $('#error-message').text('');
+    }
+
     $('#output-select').text(selectValue);
     $('#output-text').text(textValue);
     return false;
