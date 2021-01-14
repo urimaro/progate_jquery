b486fd6ebe825c568e8d81d3d4d48e11e4b23971
Goh Matsumoto <urimaro@gmail.com>
2021-01-14 23:07:47 +0900

    18. Creating a Navbar - 3. Adding Motion

--- jquery_iii/4_script.js
+++ jquery_iii/4_script.js
@@ -11,4 +11,10 @@ $(function() {
   $('#top-btn').click(function() {
     $('html, body').animate({'scrollTop': 0}, 500);
   });
+
+  $('header a').click(function() {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html, body').animate({'scrollTop': position}, 500);
+  });
 });
