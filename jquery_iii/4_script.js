0640a4c1781d2ae3d7091555dfaa27862faac111
Goh Matsumoto <urimaro@gmail.com>
2021-01-14 22:48:29 +0900

    15. Adding Animations - 3. Adding Motion

---
+++ jquery_iii/4_script.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.social-icon').hover(
+    function() {
+      $(this).animate({'font-size': '30px'}, 300);
+    },
+    function() {
+      $(this).animate({'font-size': '24px'}, 300);
+    }
+  );
+});
